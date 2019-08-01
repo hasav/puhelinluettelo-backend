2325c2f3deb9a4ce0db8a5f741a1c4a3be67a7ce
hasav <vlj.hasa@gmail.com>
2019-08-01 21:48:59 +0300

    heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
     {
