5cbf6c00dcf2623b3b6c3a226b91ae93909a9ec2
hasav <vlj.hasa@gmail.com>
2019-08-10 08:15:56 +0300

    uibuild

---
+++ build/precache-manifest.6db14d62253f9be0a70f91f65bf52ca8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba563e9bc00220e98cf5bcee2de743d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23453d3c8957e0db2e90",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c7efe630353d09fb33df",
+    "url": "/static/js/2.759a0627.chunk.js"
+  },
+  {
+    "revision": "23453d3c8957e0db2e90",
+    "url": "/static/js/main.f0e1bbbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
